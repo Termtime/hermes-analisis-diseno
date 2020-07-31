de87269d48f1f58c7cac585e30a7e2f7eeea82cc
Jonathan Sevilla <jonathansevilla.7@gmail.com>
2020-07-31 01:03:33 -0600

    Interfaz mantenimento usuario

--- src/main/java/com/unah/hermes/MantUsuariosPageController.java
+++ src/main/java/com/unah/hermes/MantUsuariosPageController.java
@@ -11,7 +11,7 @@ public class MantUsuariosPageController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // Mantenimiento usuario
+        // 
     }    
     
 }
