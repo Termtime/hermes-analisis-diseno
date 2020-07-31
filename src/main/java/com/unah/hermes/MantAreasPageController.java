174403f72436c97739c68ec0f689c403971ce1b4
EdgardoCruz <cruzedgardo96@gmail.com>
2020-07-31 12:27:43 -0600

    Inicio de los archivos .java, aunque con errores xD

--- src/main/java/com/unah/hermes/MantAreasPageController.java
+++ src/main/java/com/unah/hermes/MantAreasPageController.java
@@ -1,18 +1,49 @@
 package com.unah.hermes;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.stage.Window;
+import javafx.stage.Modality;
 
 public class MantAreasPageController implements Initializable {
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        //TODO
-        //Prueba
-    }    
-    
+
+        @FXML
+        private void btnCrearAreaClick(ActionEvent event) {
+                Parent root;
+                Window parentStage;
+                try {
+                        parentStage = ((Node) (event.getSource())).getScene().getWindow();
+                } catch (Exception e) {
+                        try {
+                                root = FXMLLoader.load(getClass().getResource("/fxml/crearArea.fxml"));
+                                Stage stage = new Stage();
+                                stage.setTitle("My New Stage Title");
+                                stage.setScene(new Scene(root));
+                                stage.initModality(Modality.APPLICATION_MODAL);
+                                stage.showAndWait();
+                                // stage.show();
+                        } catch (IOException a) {
+                                a.printStackTrace();
+                        }
+                }
+        }
+
+        @Override
+        public void initialize(URL arg0, ResourceBundle arg1) {
+                // TODO Auto-generated method stub
+
+        }
+
 }
---
+++ src/main/java/com/unah/hermes/crearAreaController.java
@@ -0,0 +1,19 @@
+package com.unah.hermes;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.unah.hermes.provider.FirebaseConnector;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
