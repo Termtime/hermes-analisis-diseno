21b761fb79d4b4c3dde49795914f9fb6a47519ac
borisavila539 <68365374+borisavila539@users.noreply.github.com>
2020-08-04 20:06:26 -0600

    avancesBoris

--- src/main/java/com/unah/hermes/MantProductosPageController.java
+++ src/main/java/com/unah/hermes/MantProductosPageController.java
@@ -6,9 +6,21 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import com.unah.hermes.utils.Navigation;
 
 public class MantProductosPageController implements Initializable {
-    
+    @FXML private void btnAgregarProductoClick(ActionEvent event) {
+        Navigation.pushRoute("Mant_Agregar_Producto", event, false, true);
+    }
+    @FXML private void btnModificarProductoClick(ActionEvent event) {
+        Navigation.pushRoute("Mant_Modificar_Producto", event, false, true);
+    }
+    @FXML private void btnAgregarCategoriaClick(ActionEvent event) {
+        Navigation.pushRoute("Mant_Agregar_Categoria", event, false, true);
+    }
+    @FXML private void btnModificarCategoriaClick(ActionEvent event) {
+        Navigation.pushRoute("Mant_Modificar_Categoria", event, false, true);
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // cambios
